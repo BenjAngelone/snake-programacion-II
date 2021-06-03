e08b9ed0068e1250a5867b7e6f056bb024239575
BenjAngelone <2008273@ucc.edu.ar>
2021-06-03 05:24:45 -0300

    una negreada que cumple  con los  requisitos

--- mian_juego.cpp
+++ mian_juego.cpp
@@ -1,51 +1,58 @@
-#ifndef __PER_INCLUDED__
-#define __PER_INCLUDED__
-#include "personaje.h"
-#endif
-#include "mago.h"
-#include "clerigo.h"
+
+#include "jugador.h"
+#include "vibora.h"
+#include "juego.h"
 #include <iostream>
 
 using namespace std;
+void Dibujar(juego A){
+    for(int i =0;i<=A.getTamaño_y();i++){
+        for(int j =0;j<=A.getTamaño_x();j++){
+            switch (A.getCodex(j,i))
+            {
+            case 0:
+                cout<<"  ";
+                break;
+            case 4||5||6||7||8:
+                cout<<"O ";
+                break;
+            case 3:
+                cout<<"| ";
+                break;
+            case -3:
+                cout<<" |"<<endl;
+                break;
+            case 2:
+                cout<<"__";
+                break;
+            case -1:
+                cout<<"@ ";
+                break;
+            case 8:
+                cout<<"* ";
+                break;
+            
+            default:
+                break;
+}
+}
+}
+}
 
 int main(){
+  char dir;
 
-    mago A("Mago_A","humano",20,20,100), B("Mago_B","humano",20,20,100);
-    clerigo C("Cler_C","humano",20,20,100,"DIOS_C");
-    cout<<"Personajes\n";
-    A.Imprime();
-    B.Imprime();
-    C.Imprime();
-
-    A.AprendeHechizo("Hechizo 1A");
-    A.AprendeHechizo("Hechizo 2A");
-
-    B.AprendeHechizo("Hechizo 1B");
-    cout<<"\nA tiene dos Hechizos\nB tiene un Hechizo\n";
-    A.Imprime();
-    B.Imprime();
-
-    cout<<"\nA Hechiza a B\n";
-    A.LanzaHechizo(B);
-    A.Imprime();
-    B.Imprime();
-
-    cout<<"\nB Hechiza a A\n";
-    B.LanzaHechizo(A);
-    A.Imprime();
-    B.Imprime();
-
-    cout<<"\nC Cura a B\n";
-    C.curar(B);
-    A.Imprime();
-    B.Imprime();
-    C.Imprime();
-
-    cout<<"\nA Hechiza a B\n";
-    A.LanzaHechizo(B);
-    A.Imprime();
-    B.Imprime();
-    C.Imprime();
-
+  juego  A(5,6);
+  vibora  V(1,1,1,1,'s','n');
+  A.Definir_tablero();
+  while (true){
+    cin>>dir;
+    V.setDirecion(dir);
+    V.obtenerDireccionCaveza(A);
+    A.ColisionDetector(V.getCabeza_x(),V.getCabeza_y());
+    V.obtenerDireccionCola(A);
+    Dibujar(A);
   }
-
+  }
+  
+    
